4932da59c96b92b0bd370a08d92d53b6a3998a8e
Muhammed Jasil <mohammedjass007@gmail.com>
2024-12-12 21:41:32 +0300

    second commit

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Frame1 from "./frame1";
 import Framep from "./framep";
+import Frame2 from "./frame2";
+
 function App(){
     return(<div className="main">
         <Frame1/>
         <Framep/>
+        <Frame2/>
         </div>);
 }
 
---
+++ src/components/frame2.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function frame2(){
+    return (
+        <section id="products">
+             <div className="frame2">
+                <div className="card1">
+                    <img className="card1img" src="/public/images/Rectangle 4.png"/>
+                    <h3 className="card1name">Gear VR Headset</h3>
+                </div>
+                <div className="card2">
+                    <img className="card2img" src="/public/images/Rectangle 5.png"/>
+                    <h3 className="card2name">Smart VR Headset</h3>
+                </div>
+                <div className="card3">
+                    <img className="card3img" src="/public/images/Rectangle 6.png"/>
+                    <h3 className="card3name">Play Station VR</h3>
+                </div>
+            </div>
+        </section>
+    );
+}
